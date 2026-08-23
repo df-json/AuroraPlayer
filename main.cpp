400d6fc521fa0422407724bd19a2328f36466ee8
mishan-karki <misshan.karki@gmail.com>
2026-08-23 10:40:37 +0545

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ class Song{
 public:
 string name;
 string artist;
-sf::Music music;
+
 string songFile;
 
 void setsonsing(string n, string a ){
@@ -18,20 +18,23 @@ void setsonsing(string n, string a ){
 }
 void setFile(const string& file) {
         songFile = file;
-        if (!music.openFromFile(songFile)) {
-            cerr << "Failed to load song: " << songFile << endl;
-        }
+
+}
+void playSong(const Song& song, sf::Music& music) {
+    if (music.openFromFile(song.songFile)) {
+        music.play();
     }
+}
 };
 class Playlist {
 private:
     string name;
-    vector<shared_ptr<Song>> songs;   // shared_ptr if songs can appear in multiple playlists
+    vector<Song> songs;   // shared_ptr if songs can appear in multiple playlists
 
 public:
     Playlist(const string& n) : name(n) {}
 
-    void addSong(shared_ptr<Song> s) {
+    void addSong(const Song& s) {
         songs.push_back(s);
     }
 
@@ -40,19 +43,33 @@ public:
             songs.erase(songs.begin() + index);
     }
 
-    void playAll() {
-        for (auto& s : songs) s->play();
+    void playSong(const Song& song, sf::Music& music) {
+    if (music.openFromFile(song.songFile)) {
+        music.play();
     }
-
+}
     string getName() const { return name; }
     size_t count() const { return songs.size(); }
 };
 
+void createplay( vector<Song> &songs){
+
+     int i;
+     if (i != 0)
+
+     Playlist playlist(songs[i].name);
+
+}
+
+
+
 int main()
 {
+    sf::Music music;
     vector<Song> songs;
     int i,n;
     string name, artist, file;
+
     cout<<"How many songs"<<endl;
     cin>>n;
     for (i = 0 ; i < n ; i ++){
@@ -67,7 +84,11 @@ int main()
         songs.push_back(move(s));
 
 
+
+
     }
+
+
 
 
 
